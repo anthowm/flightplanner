3b2a1c51259f835ecf680ea7539f3dc6756b9318
PC-EULER\awillis <>
2019-01-29 17:57:42 +0100

    feat(core): adding flealef maps

--- flightplanner/src/app/app.component.ts
+++ flightplanner/src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tileLayer, latLng, circle, polygon, marker, polyline } from 'leaflet';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'flightplanner';
+export class AppComponent implements OnInit {
+  map;
+  allDrawnItems = new L.FeatureGroup();
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...'
+      })
+    ],
+    zoom: 5,
+    center: latLng(51.9487949, 7.6237527)
+  };
+  drawOptions = {
+    position: 'bottomright',
+    draw: {
+      circlemarker: false,
+      polyline: true
+    },
+    featureGroup: this.allDrawnItems
+  };
+
+  constructor() {}
+  ngOnInit() {
+    this.options = {
+      layers: [
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 18,
+          attribution: '...'
+        })
+      ],
+      zoom: 12,
+      center: latLng(51.9487949, 7.6237527)
+    };
+    this.drawOptions = {
+      position: 'bottomright',
+      draw: {
+        circlemarker: false,
+        polyline: true
+      },
+      featureGroup: this.allDrawnItems
+    };
+  }
+
+  btn_drawPolygon() {
+    const polylineDrawer = new L.Draw.Polyline(this.map); // <-- throws error
+    polylineDrawer.enable();
+  }
+
+  onDrawReady(event) {
+    console.log(event.layer);
+  }
+
+  onMapReady(map: L.Map) {
+    console.log('ON MAP READY CALLED');
+    console.log(this.map);
+    this.map = map;
+  }
 }
--- flightplanner/src/app/app.module.ts
+++ flightplanner/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LeafletDrawModule } from '@asymmetrik/ngx-leaflet-draw';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LeafletModule.forRoot(),
+    LeafletDrawModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
